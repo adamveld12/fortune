4a1495bcce220a59c39cdef44c289ac9c6ab84c8
Adam Veldhousen <adamveld12@gmail.com>
2015-02-19 21:15:21 -0600

    uses the original format that fortune uses. Added an option to select a source file

--- main.go
+++ main.go
@@ -11,20 +11,25 @@ import (
 
 var serverUrl string
 var wait time.Duration
+var filePath string
 
 func init() {
-	flag.StringVar(&serverUrl, "server", "", "Retrieves a quote from the specified quote server")
-	flag.DurationVar(&wait, "wait", time.Nanosecond, "How long to wait before terminating the process. (eg. 1ms, 1.2s)")
+	flag.StringVar(&filePath, "file", "fortunes.txt", "Retrieves a quote from the specified quote file.")
+	flag.StringVar(&serverUrl, "server", "", "Retrieves a quote from the specified quote server.")
+	flag.DurationVar(&wait, "wait", time.Nanosecond, "How long to wait before terminating the process (eg. 1ms, 1.2s).")
 }
 
 func main() {
 	flag.Parse()
+
 	var quote string
+
 	if serverUrl != "" {
 		quote = GetQuoteFromService()
 	} else {
 		quote = GetQuoteFromFile()
 	}
+
 	fmt.Println(quote)
 	time.Sleep(wait)
 }
@@ -35,13 +40,13 @@ func GetQuoteFromService() string {
 
 func GetQuoteFromFile() string {
 
-	data, err := ioutil.ReadFile("quotes.txt")
+	data, err := ioutil.ReadFile(filePath)
 
 	if err != nil {
 		panic(err.Error())
 	}
 
-	text := strings.Split(string(data), "\n\n")
+	text := strings.Split(string(data), "%")
 
 	rand.Seed(time.Now().UnixNano())
 	line := text[rand.Intn(len(text))]
